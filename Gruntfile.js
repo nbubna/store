c6bc7399e6f1ca8a540ce2084b8efe93017e4c52
Nathan Bubna <nathan@esha.com>
2017-03-28 13:11:46 -0700

    some grunt sugar

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,12 @@
 
 module.exports = function(grunt) {
 
+  // Load grunt tasks automatically
+  require('load-grunt-tasks')(grunt);
+
+  // Time how long tasks take. Can help when optimizing build times
+  require('time-grunt')(grunt);
+
   // Project configuration.
   grunt.initConfig({
     // Metadata.
@@ -101,15 +107,8 @@ module.exports = function(grunt) {
     }
   });
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-component-build');
-  grunt.loadNpmTasks('grunt-nuget');
+  // load frame task
+  grunt.loadTasks('tasks');
 
   // Default task.
   grunt.registerTask('default', ['jshint', 'clean', 'concat', 'component_build', 'qunit', 'uglify']);

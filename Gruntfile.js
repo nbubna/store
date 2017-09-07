661acb06958846bf6912987d7683d76249a5fc8e
Nathan Bubna <nathan@esha.com>
2017-09-06 21:49:06 -0700

    sync to package license declaration change

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,7 +16,7 @@ module.exports = function(grunt) {
       '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
       '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
       '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-      ' Licensed <%= pkg.licenses.map(function(l){l.type;}).join(", ") %> */\n',
+      ' Licensed <%= pkg.license %> */\n',
     // Task configuration.
     clean: {
       files: ['dist']

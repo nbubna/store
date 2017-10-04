be824973397bf5a20f844734ca7832de51096a68
Nathan Bubna <nathan@esha.com>
2017-10-04 15:14:56 -0700

    more wrangling w/TypeScript typings

--- index.d.ts
+++ index.d.ts
@@ -1,45 +1,59 @@
-declare module 'store' {
+declare function store(key: any, fn: (data: any) => any, alt?: any): StoreAPI
+declare function store(key: any, data: any): any
+declare function store(clearIfFalsy: false | 0): StoreAPI
+declare function store(key: any): any
+declare function store(obj: Object): StoredData
+declare function store(): StoredData
 
-  interface store {
-    set(key: any, data: any, overwrite?: boolean): any;
-    setAll(data: Object, overwrite?: boolean): store.Data;
-    get(key: any, alt?: any): any;
-    getAll(): store.Data;
-    transact(key: any, fn: (data: any) => any, alt?: any): store;
-    clear(): store;
-    has(key: any): boolean;
-    remove(key: any): any;
-    each(callback: (key: any, data: any) => false | any): store;
-    keys(): string[];
-    size(): number;
-    clearAll(): store;
-    isFake(): boolean;
-    namespace(namespace: string, noSession?: true): store;
-  }
-  interface Storage {
-    getItem(key: string): string;
-    setItem(key: string, value: string): void;
-    removeItem(key: string): void;
-    key(index: number): string;
-    length: number;
-    clear(): void;
-  }
-
-  function store(key: any, data: any): any
-  function store(key: any): any
-  function store(key: any, fn: (data: any) => any, alt?: any): store
-  function store(obj: Object): store.Data
-  function store(): store.Data
-  function store(clearIfFalsy: false | 0): store
-
-  namespace store {
-    interface Data {
-      [key: string]: any;
-    }
-    const session: store;
-    const local: store;
-    function area(id: string, area: Storage): store;
-  }
-
-  export = store;
+// if anyone knows a way to avoid duplicating the StoreAPI interface here that'd be great
+declare namespace store {
+  const local: StoreAPI;
+  const session: StoreAPI;
+  function area(id: string, area: Storage): StoreAPI;
+  function set(key: any, data: any, overwrite?: boolean): any;
+  function setAll(data: Object, overwrite?: boolean): StoredData;
+  function get(key: any, alt?: any): any;
+  function getAll(): StoredData;
+  function transact(key: any, fn: (data: any) => any, alt?: any): StoreAPI;
+  function clear(): StoreAPI;
+  function has(key: any): boolean;
+  function remove(key: any): any;
+  function each(callback: (key: any, data: any) => false | any): StoreAPI;
+  function keys(): string[];
+  function size(): number;
+  function clearAll(): StoreAPI;
+  function isFake(): boolean;
+  function namespace(namespace: string, noSession?: true): StoreAPI;
 }
+
+declare interface StoreAPI {
+  set(key: any, data: any, overwrite?: boolean): any;
+  setAll(data: Object, overwrite?: boolean): StoredData;
+  get(key: any, alt?: any): any;
+  getAll(): StoredData;
+  transact(key: any, fn: (data: any) => any, alt?: any): StoreAPI;
+  clear(): StoreAPI;
+  has(key: any): boolean;
+  remove(key: any): any;
+  each(callback: (key: any, data: any) => false | any): StoreAPI;
+  keys(): string[];
+  size(): number;
+  clearAll(): StoreAPI;
+  isFake(): boolean;
+  namespace(namespace: string, noSession?: true): StoreAPI;
+}
+
+declare interface StoredData {
+  [key: string]: any;
+}
+
+declare interface Storage {
+  getItem(key: string): string;
+  setItem(key: string, value: string): void;
+  removeItem(key: string): void;
+  key(index: number): string;
+  length: number;
+  clear(): void;
+}
+
+export = store;
